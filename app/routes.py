00ec1c41482c5bf6383581c4ed0eb2a02eeb3038
00MB <s2061204@ed.ac.uk>
2021-01-31 01:51:13 -0500

    Front end without animation

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,11 @@ from app import app
 @app.route('/index')
 def index():
     return render_template('lander.html')
+
+@app.route('/photo')
+def photomenu():
+    return render_template('photo.html')
+
+@app.route('/output')
+def output():
+    return render_template('output.html', infected = "True", probabilities = {"Healthy": 10, "CMD": 20, "CGM": 40, "CBSD": 80, "CBB": 40})
